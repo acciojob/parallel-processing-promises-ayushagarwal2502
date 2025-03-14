009f7ae3de99276d05b8ab15fe7ab30fd18d47e1
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-03-14 12:31:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 //your JS code here. If required.
-
 window.onload = function() {
-  //const btn = document.getElementById("download-images-button");
-   const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
-const images = [
-  "https://picsum.photos/id/237/200/300",
-  "https://picsum.photos/id/238/200/300",
-  "https://picsum.photos/id/239/200/300"
-];
+  const output = document.getElementById("output");
+  const btn = document.getElementById("download-images-button");
+  const images = [
+    "https://picsum.photos/id/237/200/300",
+    "https://picsum.photos/id/238/200/300",
+    "https://picsum.photos/id/239/200/300"
+  ];
 
-function downloadImage(url) {
-  return new Promise((resolve, reject) => {
-    let img = new Image();
-    img.src = url;
-    img.onload = () => resolve(img);
-    img.onerror = () => reject(new Error(`Failed to load image at url ${url}`));
-  });
-}
+  function downloadImage(url) {
+    return new Promise((resolve, reject) => {
+      let img = new Image();
+      img.src = url;
+      img.onload = () => resolve(img);
+      img.onerror = () => reject(new Error(`Failed to load image at url ${url}`));
+    });
+  }
 
-function downloadAllImages(urls) {
-  let imagePromises = urls.map(downloadImage);
-  return Promise.all(imagePromises);
-}
+  function downloadAllImages(urls) {
+    let imagePromises = urls.map(downloadImage);
+    return Promise.all(imagePromises);
+  }
 
-downloadAllImages(images)
-  .then(images => {
-    images.forEach(img => document.body.appendChild(img));
-  })
-  .catch(err => {
-    console.error(err);
+  btn.addEventListener('click', function() {
+    downloadAllImages(images)
+      .then(images => {
+        images.forEach(img => output.appendChild(img));
+      })
+      .catch(err => {
+        console.error(err);
+      });
   });
 }
