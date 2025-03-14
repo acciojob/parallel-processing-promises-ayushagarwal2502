d7fc830eefdcec9f8b2fa558d941699fe2b46e3b
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-03-14 12:22:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 //your JS code here. If required.
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
-
 const images = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
+  "https://picsum.photos/id/237/200/300",
+  "https://picsum.photos/id/238/200/300",
+  "https://picsum.photos/id/239/200/300"
 ];
-function downloadImage(url){
-	return new Promise((resolve,reject)=>{
-	img.src = url;
+
+function downloadImage(url) {
+  return new Promise((resolve, reject) => {
+    let img = new Image();
+    img.src = url;
     img.onload = () => resolve(img);
     img.onerror = () => reject(new Error(`Failed to load image at url ${url}`));
   });
 }
+
 function downloadAllImages(urls) {
   let imagePromises = urls.map(downloadImage);
   return Promise.all(imagePromises);
 }
-let urls = ["url1", "url2", "url3"];
-downloadAllImages(urls)
+
+downloadAllImages(images)
   .then(images => {
     images.forEach(img => document.body.appendChild(img));
   })
