c0f34dea146e6d12bc9aca1042ea0bfa8e9436f3
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-03-14 12:17:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+function downloadImage(url){
+	return new Promise((resolve,reject)=>{
+	img.src = url;
+    img.onload = () => resolve(img);
+    img.onerror = () => reject(new Error(`Failed to load image at url ${url}`));
+  });
+}
+function downloadAllImages(urls) {
+  let imagePromises = urls.map(downloadImage);
+  return Promise.all(imagePromises);
+}
+let urls = ["url1", "url2", "url3"];
+downloadAllImages(urls)
+  .then(images => {
+    images.forEach(img => document.body.appendChild(img));
+  })
+  .catch(err => {
+    console.error(err);
+  });
